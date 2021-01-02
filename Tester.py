414fc776a7cab19204f2bfedb6579d04c8cf4ed2
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-01-02 06:59:35 -0600

    Some 'UI' improvements

--- Tester.py
+++ Tester.py
@@ -1,9 +1,19 @@
 from io import StringIO 
+from time import sleep
 
 try:
     import requests
 except ModuleNotFoundError:
-    print("'Tester' needs 'requests' module to work.")
+    
+    def _fkLoading():
+        for _ in range(4):
+            print("*", end="")
+            sleep(0.5)
+
+    _fkLoading()
+    print(" 'Tester' needs 'requests' module to work.")
+    
+    _fkLoading()
     print("Installing 'requests' module...\n")
     
     try:
@@ -14,24 +24,24 @@ except ModuleNotFoundError:
                 pip.main(['install', package])
             else:
                 pip._internal.main(['install', package])
-            
 
         install('requests')
 
         try:
             import requests
+            _fkLoading()
             print("'requests' module installed successfully")
         except ModuleNotFoundError:
-            print("Something went wrong while updating Tester [Line 25]\n")
+            print("Something went wrong while updating Tester [Line 35]\n")
         
         
     except:
-        print("Something went wrong while updating Tester [Line 29]")
+        print("Something went wrong while updating Tester [Line 39]")
 
 import sys
 import os
 
-version : float = 0.111
+version : float = 0.12
 
 class _Capturing(list):
     def __enter__(self):
@@ -63,6 +73,8 @@ class test1():
         print(output, testResult.lower())
         if testResult.lower() in [i.lower() for i in output]:
             print("Correcto")
+        else:
+            print("Incorrecto :D")
     
     def printTest():
         print("Una función que por medio de inputs (3) reciba un nombre [Daniel], una edad [27] y un país de origen [España] e imprima el mensaje 'Hola mi nombre es [NOMBRE], tengo [EDAD] años y vengo de [PAIS]'.")
@@ -78,7 +90,12 @@ class _UpdateManager():
                 gitVersion += i
 
         return float(gitVersion)
-
+    
+    @staticmethod
+    def _fkLoading():
+        for _ in range(4):
+            print("*", end="")
+            sleep(0.5)
 
     @staticmethod
     def update():
@@ -86,16 +103,21 @@ class _UpdateManager():
         try:
             gitVersion : float = _UpdateManager._getRepoVersion()
         except:
-            print("Failed to get GitHub version [Line 89]")
+            print("Failed to get GitHub version [Line 104]")
             return 
 
         if gitVersion != version and gitVersion > version:
             try:
-                print(f"Actualizando 'Tester' v. {version} -> {gitVersion}...")
+                for _ in range(4):
+                    print("*", end="")
+                    sleep(0.5)
+                
+                _UpdateManager._fkLoading()
+                print(f" Actualizando 'Tester' v. {version} -> {gitVersion}...")
                 os.system("curl -sS https://raw.githubusercontent.com/AOx0/py-ColoTester/master/Tester.py -o Tester.py")
                 print("Tester actualizado!")
             except:
-                print("Something went wrong while updating Tester [Line 98]")
+                print("Something went wrong while updating Tester [Line 118]")
                 return
 
 
